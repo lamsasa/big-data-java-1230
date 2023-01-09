5ce0cb838736e75fdd4ed329c9bcd1096e44ad8a
dahye <dh85311@gmail.com>
2023-01-09 09:52:02 +0900

    9일

--- src/상근날드/SetMenu.java
+++ src/상근날드/SetMenu.java
@@ -11,6 +11,22 @@ import java.util.Scanner;
 public class SetMenu {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int [] arr = {1, 2, 3};
+        int [] menu = new int[5]; // 5개의 정수로 구성된 배열 생성
+        System.out.println("메뉴 입력 : "); // 화면 출력, 햄버거 3개와 음료 2개의 가격을 연속으로 입력 받음
+        for(int i = 0; i < menu.length; i++) {
+            menu[i] = sc.nextInt(); //배열의 길이 만큼 순회하면서 값을 연속으로 입력 받음
+        }
+        int minBurger =menu[0]; // 0 ~ 2 인덱스 사이의 햄버거 중 하나의 값을 기본값으로 설정
+        int minDrink = menu[3]; // 3 ~ 4 인덱스 사이의 음료 중 하나의 값을 기본값으로 설정
+        //반복문을 순회하면서 햄버거 1 개와 음료 1 개 선택(제일 싼 거)
+        for(int i = 0; i < menu.length; i++){
+            if((i < 3) &&  (minBurger > menu[i])) {
+                minBurger = menu[i];
+            }
+            if ((i > 2) && (minDrink > menu[i])) {
+                minDrink = menu[i];
+            }
+        }
+        System.out.println(minBurger + minDrink - 50);
     }
 }
